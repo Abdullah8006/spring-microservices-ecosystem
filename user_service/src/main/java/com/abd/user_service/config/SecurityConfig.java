be919119d6c839f8af5242f0aa3a7b62b0628fdb
Abdullah8006 <abdullah.khan8006@gmail.com>
2025-10-07 14:04:17 +0300

    Added Oauth resource server config for keycloak

---
+++ user_service/src/main/java/com/abd/user_service/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.abd.user_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/public/**").permitAll() // Allow public access to /public/**
+                        .anyRequest().authenticated() // Require authentication for all other requests
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults()) // Enable JWT-based resource server
+                );
+        return http.build();
+    }
+
+}
